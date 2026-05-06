c71b77024f5e1f247c5a4b132d08186101690442
Jérôme Chabot <jayjaychabot@gmail.com>
2026-05-05 22:17:33 -0400

    Fix

--- src/AnswerBox.tsx
+++ src/AnswerBox.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
 import { motion } from 'framer-motion';
+import React from 'react';
 import type { Answer } from './types';
-import { playBellSound } from './audio';
 
 interface AnswerBoxProps {
   answer: Answer | null;
